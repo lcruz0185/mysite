5e6f6e098667152f9a131f9f4ddbdddd6921d894
Loraine Cruz <lcruz0185@24dcaea7d349>
2018-04-24 15:30:15 +0000

    Converted About Me and Register pages to templates that extend base.html

--- flask_app.py
+++ flask_app.py
@@ -13,7 +13,7 @@ def hello_world():
 
 @app.route('/about_me')
 def about_me():
-    return app.send_static_file('about_me.html')
+    return render_template('about_me.html')
 
 @app.route('/class_schedule')
 def class_schedule():
@@ -21,5 +21,5 @@ def class_schedule():
                            courses=constants.COURSES)
 @app.route('/register')
 def register():
-    return app.send_static_file('register.html')
+    return render_template('register.html')
 
