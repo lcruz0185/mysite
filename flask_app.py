b505f780f3e63289212840d13af04b01af8c1a96
Loraine Cruz <lcruz0185@24dcaea7d349>
2018-05-03 16:10:36 +0000

    Define the course table for the database.

--- flask_app.py
+++ flask_app.py
@@ -11,6 +11,14 @@ app = Flask(__name__)
 app.config.from_object('config.BaseConfig')
 db = SQLAlchemy(app)
 
+class Course(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    period = db.Column(db.Integer)
+    name = db.Column(db.String(80))
+    teacher_name = db.Column(db.String(80))
+    resource_name = db.Column(db.String(80))
+    resource_url = db.Column(db.String(300))
+
 @app.route('/')
 def homepage():
     return render_template('index.html')
@@ -31,4 +39,5 @@ def register():
 def top_ten_songs():
     return render_template('top_ten_songs.html',songs=constants.TOP_TEN_SONGS)
 
-
+if __name__ == '__main__':
+  db.create_all()
