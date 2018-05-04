f71de2691080499c0f69890b54b3d1319339627a
Loraine Cruz <lcruz0185@24dcaea7d349>
2018-05-04 15:49:22 +0000

    Moved course information into the database.

--- flask_app.py
+++ flask_app.py
@@ -29,8 +29,10 @@ def about_me():
 
 @app.route('/class_schedule')
 def class_schedule():
+    courses = Course.query.all()
     return render_template('class_schedule.html',
-                           courses=constants.COURSES)
+                           courses=courses)
+
 @app.route('/register')
 def register():
     return render_template('register.html')
